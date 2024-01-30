b4f997f220a61ad75bbe86ddf73ff86095e4fe86
Ayush Chandola <155428667+potataahhh@users.noreply.github.com>
2024-01-31 01:17:15 +0530

    Create linear search.cpp

---
+++ Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/Step 3.1 : Easy/linear search.cpp
@@ -0,0 +1,11 @@
+int linearSearch(int n, int num, vector<int> &arr)
+{
+    // Write your code here.
+
+    for(int i=0;i<n;i++){
+        if(num==arr[i]){
+            return i;
+        }
+    }
+    return -1;
+}
