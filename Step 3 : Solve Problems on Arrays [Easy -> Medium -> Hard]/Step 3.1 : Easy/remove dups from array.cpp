0a0de0a3df1c5f255fce7f8b13682703b92933f3
Ayush Chandola <155428667+potataahhh@users.noreply.github.com>
2024-01-31 00:53:19 +0530

    Create remove dups from array.cpp

---
+++ Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/Step 3.1 : Easy/remove dups from array.cpp
@@ -0,0 +1,12 @@
+int removeDuplicates(vector<int> &arr, int n) {
+	// Write your code here.
+
+	int count =1;
+
+	for(int i=1;i<n;i++){
+		if(arr[i]!=arr[i-1]){
+		count++;
+		}
+	}
+	return count;
+}
